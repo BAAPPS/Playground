2d55e761b8542c0dac7a368fafa32dcd0ee9e4a5
BAAPPS <208905508+BAAPPS@users.noreply.github.com>
2025-07-01 07:17:35 -0700

    feat(bookworm): initial commit of Bookworm app

---
+++ SwiftUI_Learning/Project_11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by D F on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_Learning/Project_11/Bookworm/README.md
@@ -0,0 +1,5 @@
+# Bookworm Summary
+
+---
+
+## Day 53 – Project 11, part one
